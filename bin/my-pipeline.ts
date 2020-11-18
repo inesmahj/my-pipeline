5522a5316a98fa17ee7dcafbd956307b265cf13d
Ines Mahjoub <Ines.Mahjoub@draexlmaier.com>
2020-11-18 09:27:43 +0100

    initial commit

--- bin/my-pipeline.ts
+++ bin/my-pipeline.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
-import { MyPipelineStack } from '../lib/my-pipeline-stack';
+import { PipelineStack } from '../lib/my-pipeline-stack';
 
 const app = new cdk.App();
-new MyPipelineStack(app, 'PipelineStack', {
+new PipelineStack(app, 'PipelineStack', {
   env: {
     account: '299776909111',
     region: 'eu-central-1',
--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -1,39 +1,44 @@
 import { Stack, StackProps, Construct, SecretValue } from '@aws-cdk/core';
 import { CdkPipeline, SimpleSynthAction } from '@aws-cdk/pipelines';
-
+import * as cdk from '@aws-cdk/core';
 import * as codepipeline from '@aws-cdk/aws-codepipeline';
 import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
 
-export class MyPipelineStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
+
+
+export class PipelineStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props: cdk.StageProps) {
     super(scope, id, props);
 
+    // Create CodePipeline artifact to hold source code from repo
     const sourceArtifact = new codepipeline.Artifact();
+    // Create CodePipeline artifact to hold synthesized cloud assembly
     const cloudAssemblyArtifact = new codepipeline.Artifact();
 
+    // Create the CDK pipeline
     const pipeline = new CdkPipeline(this, 'Pipeline', {
-      pipelineName: 'MyAppPipeline',
+      pipelineName: 'ServerlessPipelineDemo',
       cloudAssemblyArtifact,
 
+      // Checkout source from GitHub
       sourceAction: new codepipeline_actions.GitHubSourceAction({
-        actionName: 'GitHub',
-        output: sourceArtifact,
-        oauthToken: SecretValue.secretsManager('github-tokens'),
-        trigger: codepipeline_actions.GitHubTrigger.POLL,
-        // Replace these with your actual GitHub project info
+        actionName: 'Source',
         owner: 'inesmahj',
         repo: 'my-pipeline',
+       
+        oauthToken: cdk.SecretValue.secretsManager('github-tokens'),
+        output: sourceArtifact,
       }),
- 
-
-
+      // For synthesize we use the default NPM synth
       synthAction: SimpleSynthAction.standardNpmSynth({
         sourceArtifact,
         cloudAssemblyArtifact,
-
-        // Use this if you need a build step (if you're not using ts-node
-        // or if you have TypeScript Lambdas that need to be compiled).
-        buildCommand: 'npm run build',
+        // We override the default install command to prepare our lambda too
+        installCommand: 'npm ci && npm ci --prefix lambda',
+        // As we may need Docker we create a privileged container
+        environment: {
+          privileged: true,
+        },
       }),
     });
   }
